703087fd332837a3903108c82d85c5fa82d86590
Abdullah Khan <abdullah.khan@proxima.capital>
2022-08-16 12:28:49 +1000

    gonna deploy on gcloud

--- pb-api/main.go
+++ pb-api/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"net/http"
-
 	getblogpreviews "pb-api/src/blog"
 	getprojects "pb-api/src/projects"
 	getworkexperience "pb-api/src/work"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
@@ -19,12 +16,6 @@ func main() {
 
 	lRouter := gin.New()
 	lRouter.Use(gin.Logger())
-	lRouter.LoadHTMLGlob("templates/*.tmpl.html")
-	lRouter.Static("/static", "static")
-
-	lRouter.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
 
 	lRouter.GET("/work", getworkexperience.GetWorkExperience)
 	lRouter.GET("/projects", getprojects.GetProjects)
